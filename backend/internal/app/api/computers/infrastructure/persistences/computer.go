401fdd2057704d3c8325ca950898773fdeafae97
atsumarukun <atys.smz@gmail.com>
2024-02-16 16:27:04 +0000

    バックエンドでcomputerPersistenceのレシーバをアンダースコアから変更.

--- backend/internal/app/api/computers/infrastructure/persistences/computer.go
+++ backend/internal/app/api/computers/infrastructure/persistences/computer.go
@@ -15,22 +15,22 @@ func NewComputerPersistence() repositories.ComputerRepository {
 	return &computerPersistence{}
 }
 
-func (_ computerPersistence) Create(db *gorm.DB, computer *entities.Computer) error {
+func (cp computerPersistence) Create(db *gorm.DB, computer *entities.Computer) error {
 	return db.Create(computer).Error
 }
 
-func (_ computerPersistence) Update(db *gorm.DB, computer *entities.Computer) error {
+func (cp computerPersistence) Update(db *gorm.DB, computer *entities.Computer) error {
 	return db.Save(computer).Error
 }
 
-func (_ computerPersistence) Delete(db *gorm.DB, computer *entities.Computer) error {
+func (cp computerPersistence) Delete(db *gorm.DB, computer *entities.Computer) error {
 	return db.Unscoped().Delete(computer).Error
 }
 
-func (_ computerPersistence) FindOneById(db *gorm.DB, computer *entities.Computer, id uint) error {
+func (cp computerPersistence) FindOneById(db *gorm.DB, computer *entities.Computer, id uint) error {
 	return db.First(computer, id).Error
 }
 
-func (_ computerPersistence) Find(db *gorm.DB, computers *[]entities.Computer, options *database.Options) error {
+func (cp computerPersistence) Find(db *gorm.DB, computers *[]entities.Computer, options *database.Options) error {
 	return db.Order(fmt.Sprintf("%s %s", options.Sort, options.Order)).Find(computers).Error
 }
